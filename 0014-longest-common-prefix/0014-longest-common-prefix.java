ebe8bd3c71d35bc1437db70d1ab594cd03e107b5
Shivansh1205 <144582337+Shivansh1205@users.noreply.github.com>
2025-05-15 00:36:41 +0530

    Time: 1 ms (64.1%), Space: 41.3 MB (87.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,7 +3,7 @@ class Solution {
         if (strs == null || strs.length == 0) return "";
 
         for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
+            final char c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
                 if (i == strs[j].length() || strs[j].charAt(i) != c) {
                     return strs[0].substring(0, i);
