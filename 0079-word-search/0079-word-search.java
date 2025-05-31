bb815e1b7f9f797d40743c31ed4f42277cafa4c0
Shivansh1205 <144582337+Shivansh1205@users.noreply.github.com>
2025-05-31 21:10:15 +0530

    Time: 128 ms (90.45%), Space: 41.7 MB (63.4%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -13,26 +13,26 @@ class Solution {
     }
 
     private boolean dfs(char[][] board, String word, int i, int j, int idx) {
-        // If index matches word length, found the word
+
         if (idx == word.length()) return true;
 
-        // Boundary & character match check
+
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length ||
             board[i][j] != word.charAt(idx)) {
             return false;
         }
 
         char temp = board[i][j];
-        board[i][j] = '#'; // Mark as visited
+        board[i][j] = '#'; 
 
-        // Check all four directions
+
         boolean found =
             dfs(board, word, i + 1, j, idx + 1) ||
             dfs(board, word, i - 1, j, idx + 1) ||
             dfs(board, word, i, j + 1, idx + 1) ||
             dfs(board, word, i, j - 1, idx + 1);
 
-        board[i][j] = temp; // Restore
+        board[i][j] = temp;
 
         return found;
     }
