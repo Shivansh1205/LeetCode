ae8aee63ec8af6f5611dbaa5a44ffc5f13cd7954
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-01-03 16:46:01 +0530

    Time: 6 ms (67.09%), Space: 47.2 MB (65.03%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,57 @@
+// class Solution {
+//     public int minFallingPathSum(int[][] g) {
+//         int m = g.length;
+//         int n = g[0].length;
+//         int ans = (int)1e9;
+//         int[][]dp = new int[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++) dp[i][j] =  -101;
+//         }
+//         for(int j=0;j<n;j++) dp[0][j] = g[0][j];
+//         for(int i=1;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 int u = dp[i-1][j];
+//                 int l = (int)1e9,r=(int)1e9;
+//                 if(j>0){
+//                     l=dp[i-1][j-1];
+//                 }
+//                 if(j<n-1){
+//                     r=dp[i-1][j+1];
+//                 }
+//                 dp[i][j] = g[i][j]+Math.min(u,Math.min(l,r));
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             ans = Math.min(ans,dp[m-1][i]);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int minFallingPathSum(int[][] g) {
         int m = g.length;
         int n = g[0].length;
         int ans = (int)1e9;
-        int[][]dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++) dp[i][j] =  -101;
-        }
-        for(int j=0;j<n;j++) dp[0][j] = g[0][j];
+        int[] front = new int[n];
+        int[] cur = new int[n];
+        for(int j=0;j<n;j++) front[j] = g[0][j];
         for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
-                int u = dp[i-1][j];
+                int u = front[j];
                 int l = (int)1e9,r=(int)1e9;
                 if(j>0){
-                    l=dp[i-1][j-1];
+                    l=front[j-1];
                 }
                 if(j<n-1){
-                    r=dp[i-1][j+1];
+                    r=front[j+1];
                 }
-                dp[i][j] = g[i][j]+Math.min(u,Math.min(l,r));
+                cur[j] = g[i][j]+Math.min(u,Math.min(l,r));
             }
+            front = cur;
+            cur = new int[n];
         }
         for(int i=0;i<n;i++){
-            ans = Math.min(ans,dp[m-1][i]);
+            ans = Math.min(ans, front[i]);
         }
         return ans;
     }
