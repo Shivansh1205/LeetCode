8e63cd9108401d21d25fe3026f7e92d29f9dc88c
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-02-01 16:26:37 +0530

    Time: 1 ms (99.41%) | Memory: 42.8 MB (59.42%) - LeetSync

---
+++ 4194-count-monobit-integers/count-monobit-integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countMonobit(int n) {
+        int ans=1;
+        int x=1;
+        while(x<=n){
+            ans++;
+            x= (x<<1)|1;
+        }
+        return ans;
+    }
+}
