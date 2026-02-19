1e5e9cd7b9c137b4dec65e2ca84d2954be7eaee7
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-02-19 11:40:14 +0530

    Time: 2 ms (12%), Space: 46.3 MB (42.63%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int [][]dp = new int[prices.length+1][2];
+       
+//         for(int[] a:dp) Arrays.fill(a,-1); 
+//         int ans = f(0,1,prices,dp);
+//         return ans;
+//     }
+//     private int f(int ind, int buy,int[] a,int[][]dp){
+//         if(ind==a.length) return 0;
+//         if(dp[ind][buy]!=-1) return dp[ind][buy];
+//         int profit=0;
+//         if(buy==1){
+//             profit = Math.max(-a[ind]+f(ind+1,0,a,dp),f(ind+1,1,a,dp));
+//         }else{
+//             profit = Math.max(a[ind]+f(ind+1,1,a,dp),f(ind+1,0,a,dp));
+//         }
+//         return dp[ind][buy]=profit;
+//     }
+// }
 class Solution {
     public int maxProfit(int[] prices) {
         int [][]dp = new int[prices.length+1][2];
-       
-        for(int[] a:dp) Arrays.fill(a,-1); 
-        int ans = f(0,1,prices,dp);
-        return ans;
-    }
-    private int f(int ind, int buy,int[] a,int[][]dp){
-        if(ind==a.length) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        int profit=0;
-        if(buy==1){
-            profit = Math.max(-a[ind]+f(ind+1,0,a,dp),f(ind+1,1,a,dp));
-        }else{
-            profit = Math.max(a[ind]+f(ind+1,1,a,dp),f(ind+1,0,a,dp));
+        for(int i=prices.length-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    dp[i][j] = Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }else{
+                    dp[i][j] = Math.max(+prices[i]+dp[i+1][1], dp[i+1][0]);
+                }
+            }
         }
-        return dp[ind][buy]=profit;
+        int ans = dp[0][1];
+        return ans;
     }
 }
