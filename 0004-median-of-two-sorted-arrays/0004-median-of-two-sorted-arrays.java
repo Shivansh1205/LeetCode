c2b47e5e8db75d580bd308a5a8e0e1361f43da05
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-04-05 18:34:10 +0530

    Time: 2 ms (47.13%), Space: 48.6 MB (94.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,41 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int ind1 = n / 2;
-        int ind2 = ind1 - 1;
-        int ind1el = -1, ind2el = -1;
-        int cnt = 0, i = 0, j = 0;
-
-        while (i < n1 && j < n2) {
-            int val;
+        int[] arr = new int[nums1.length + nums2.length];
+        
+        int i = 0, j = 0, k = 0;
+        
+        // merge
+        while (i < nums1.length && j < nums2.length) {
             if (nums1[i] <= nums2[j]) {
-                val = nums1[i++];
+                arr[k++] = nums1[i++];
             } else {
-                val = nums2[j++];
+                arr[k++] = nums2[j++];
             }
-
-            if (cnt == ind1) ind1el = val;
-            if (cnt == ind2) ind2el = val;
-            cnt++;
         }
-
-        while (i < n1) {
-            int val = nums1[i++];
-            if (cnt == ind1) ind1el = val;
-            if (cnt == ind2) ind2el = val;
-            cnt++;
+        
+        while (i < nums1.length) {
+            arr[k++] = nums1[i++];
         }
-
-        while (j < n2) {
-            int val = nums2[j++];
-            if (cnt == ind1) ind1el = val;
-            if (cnt == ind2) ind2el = val;
-            cnt++;
+        
+        while (j < nums2.length) {
+            arr[k++] = nums2[j++];
         }
-
-        if (n % 2 == 1) return ind1el;
-        return (ind1el + ind2el) / 2.0;
+        
+        int total = arr.length;
+        
+        // median
+        if (total % 2 == 0) {
+            return (arr[total/2 - 1] + arr[total/2]) / 2.0;
+        } else {
+            return arr[total/2];
+        }
     }
 }
