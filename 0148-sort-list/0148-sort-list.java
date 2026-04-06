77c3801cd53132766c79a8496fd7ebfba75a6731
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-04-06 22:02:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,20 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+    // Function to merge two sorted linked lists
+    public ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+        // Create a dummy ListNode
+        ListNode dummyListNode = new ListNode(-1, null);
 
-    ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
-        ListNode dummyListNode = new ListNode(-1);
+        // Temp pointer to build merged list
         ListNode temp = dummyListNode;
 
+        // Traverse both lists
         while (list1 != null && list2 != null) {
+            // Choose smaller ListNode
             if (list1.val <= list2.val) {
                 temp.next = list1;
                 list1 = list1.next;
@@ -22,47 +17,62 @@ class Solution {
                 temp.next = list2;
                 list2 = list2.next;
             }
-            temp = temp.next; 
+            // Move temp pointer
+            temp = temp.next;
         }
 
+        // Attach remaining ListNodes
         if (list1 != null) {
             temp.next = list1;
         } else {
             temp.next = list2;
         }
+
+        // Return head of merged list
         return dummyListNode.next;
     }
 
-    ListNode findMiddle(ListNode head) {
+    // Function to find middle of linked list
+    public ListNode findMiddle(ListNode head) {
+        // If list empty or single ListNode
         if (head == null || head.next == null) {
             return head;
         }
 
+        // Slow and fast pointers
         ListNode slow = head;
         ListNode fast = head.next;
 
+        // Move fast twice as fast as slow
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
+        // Return middle ListNode
         return slow;
     }
 
+    // Function to perform merge sort
     public ListNode sortList(ListNode head) {
+        // Base case: empty or single ListNode
         if (head == null || head.next == null) {
             return head;
         }
 
+        // Find middle ListNode
         ListNode middle = findMiddle(head);
 
+        // Split into two halves
         ListNode right = middle.next;
         middle.next = null;
         ListNode left = head;
 
+        // Recursively sort both halves
         left = sortList(left);
         right = sortList(right);
 
+        // Merge sorted halves
         return mergeTwoSortedLinkedLists(left, right);
     }
 }
