85423afcf9564ee13dd68fc53b4de97faa281547
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-07-29 18:18:26 +0530

    Time: 27 ms (22.65%) | Memory: 62.1 MB (5.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,20 +1,28 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int[] a: dp){
-            Arrays.fill(a,-1);
-        }
-        int ans = f(text1.length()-1,text2.length()-1,text1,text2,dp);
+        Integer[][] dp = new Integer[text1.length()+1][text2.length()+1];
+        int ans  = f(text1,text2,text1.length(), text2.length(),dp);
         return ans;
-    }
-    private int f(int ind1,int ind2, String s1, String s2,int[][] dp){
-        if(ind1<0 || ind2<0) return 0;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s1.charAt(ind1) == s2.charAt(ind2))return 1+ f(ind1-1,ind2-1,s1,s2,dp);
-        
-        return dp[ind1][ind2] = Math.max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
-        
+        // StringBuilder sb = new StringBuilder();
+        // while(i>=0 && j>=0){
+        //         if(text1.charAt(i-1) == text2.charAt(j-1)){
+        //             sb.append(text1.charAt(i-1))
 
-
+        //         }else if(dp[i-1][j] >= dp[j-1][i]){
+        //             i--;
+        //         }else j--;
+        // }
+        
     }
+    private int f(String s1, String s2, int i, int j,Integer[][] dp){
+        if(i<=0 || j<=0) return 0;
+        if(dp[i][j] != null) return dp[i][j];
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
+            dp[i][j] = 1 + f(s1,s2,i-1,j-1,dp);
+        }
+        else{
+            dp[i][j] = Math.max(f(s1,s2,i-1,j,dp),f(s1,s2,i,j-1,dp));
+        }
+        return (int)dp[i][j];
+    }   
 }
