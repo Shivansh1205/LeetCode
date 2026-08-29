190df7b79e5d2e0eedb1421bb3bc9ff8fbabb9c0
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-08-29 09:06:50 +0530

    Time: 1 ms (43.38%), Space: 42.5 MB (7.71%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,19 +2,21 @@ class Solution {
     public int integerBreak(int n) {
         if(n==2) return 1;
         if(n==3) return 2;
-        int ans = f(n,1);
+        Integer[][] dp = new Integer[n+1][n+1];
+        int ans = f(n,1,dp);
         return ans;
     }
-    private int f(int n, int i){
+    private int f(int n, int i,Integer[][] dp){
         if(n==0){
             return 1;
         }
         if(i>n) return Integer.MIN_VALUE;
+        if(dp[n][i]!=null) return dp[n][i];
         int p =-1;
         if(i<=n){
-            p = i*f(n-i,i);
+            p = i*f(n-i,i,dp);
         }
-        int np = f(n,i+1);
-        return Math.max(p,np);
+        int np = f(n,i+1,dp);
+        return dp[n][i] = Math.max(p,np);
     }
 }
