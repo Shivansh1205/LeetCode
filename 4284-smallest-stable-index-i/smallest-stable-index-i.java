41af28c04998d2a3cf43881410229b0d20341743
Shivansh Bhageria <144582337+Shivansh1205@users.noreply.github.com>
2026-09-04 13:51:13 +0530

    Time: 1 ms (99.79%) | Memory: 46.6 MB (15.74%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,27 +1,20 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-
-        int[] prefixMax = new int[n];
-        int[] suffixMin = new int[n];
-
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, nums[i]);
-            prefixMax[i] = maxi;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        pre[0] = nums[0];
+        suf[n-1] = nums[n-1];
+        for(int i=1;i<n;i++){
+            pre[i]  = Math.max(pre[i-1],nums[i]);
         }
-
-        int mini = Integer.MAX_VALUE;
-        for (int i = n - 1; i >= 0; i--) {
-            mini = Math.min(mini, nums[i]);
-            suffixMin[i] = mini;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = Math.min(nums[i],suf[i+1]);
         }
-
-        for (int i = 0; i < n; i++) {
-            int score = prefixMax[i] - suffixMin[i];
-            if (score <= k) return i;
+        for(int i=0;i<n;i++){
+            if(pre[i]-suf[i]<=k) return i;
         }
-
         return -1;
+
     }
 }
